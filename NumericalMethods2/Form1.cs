9c4339589bc5ac23ae08ff6cc4a0965b39c9bbc7
Danis222 <danisvil@mail.ru>
2022-12-18 18:28:10 +0300

    Конечный вариант 1 лабы

--- NumericalMethods2/Form1.cs
+++ NumericalMethods2/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using LiveChartsCore;
@@ -8,70 +9,95 @@ using LiveChartsCore.Defaults;
 
 namespace NumericalMethods2
 {
-    delegate double Foo(double x);
     public partial class Form1 : Form
     {
-        private double a = 0.4, b = 4, eps = 0.000001;
-        private int numNodes = 7;
-        private int numOfPoints = 13;
-        private List<List<double>> points;
-        private List<double> diffs = new List<double>();
-        private ObservablePoint[] newton;
+        private decimal a = 0.4m, b = 4, eps = 0.000001m;
+        private int numNodes = 6;
+        private int numOfPoints = 11;
+
+        private decimal maxError = 0;
+
+        private List<List<decimal>> points;
+        private List<decimal> diffs = new List<decimal>();
         public Form1()
         {
             InitializeComponent();
-            Process();
+            List<List<decimal>> me = new List<List<decimal>>();
+            for (int i = 1; i <= 40; i++)
+            {
+                maxError = 0;
+                numNodes = i;
+                Process();
+                me.Add(new List<decimal> { i, maxError });
+            }
+            ParseVector(me);
+        }
+        private void ParseVector(ObservablePoint[] v)
+        {
+            try
+            {
+                using(StreamWriter file = new StreamWriter("data.txt"))
+                {
+                    for (int i = 0; i < v.Length; i++)
+                    {
+                        file.WriteLine(v[i].X + "\t" + v[i].Y);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(ex.Message);
+            }
+        }
+        private void ParseVector(List<List<decimal>> v)
+        {
+            try
+            {
+                using (StreamWriter file = new StreamWriter("data.txt"))
+                {
+                    for (int i = 0; i < v.Count; i++)
+                    {
+                        file.WriteLine(v[i][0] + "\t" + v[i][1]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException(ex.Message);
+            }
         }
         private void Process()
         {
-            points = Tabulate(a, b, numNodes, eps);
+            if (numNodes == 1) points = new List<List<decimal>> { new List<decimal> { a, Taylor(a) } };
+            else points = TabulateChebishev(a, b, numNodes, eps);
+
+            diffs.Clear();
             for (int i = 0; i < numNodes - 1; i++)
             {
                 diffs.Add(DividedDiffs(points.GetRange(0, i + 2)));
             }
 
-            newton = new ObservablePoint[numOfPoints];
-            for (int i = 0; i < numOfPoints; i++)
-            {
-                double step = (b - a) / numOfPoints;
-                double x = a + i * step;
-                newton[i] = new ObservablePoint(x, Newton(x, diffs, points));
-            }
-            label1.Text = numNodes.ToString();
+            decimal step = (b - a) / (numOfPoints - 1);
 
+            var newton = new ObservablePoint[numOfPoints];
             var taylor = new ObservablePoint[numOfPoints];
-            for (int i = 0; i < numOfPoints; i++)
-            {
-                double step = (b - a) / numOfPoints;
-                double x = a + i * step;
-                taylor[i] = new ObservablePoint(x, Taylor(x));
-            }
-
             var lagrange = new ObservablePoint[numOfPoints];
-            for (int i = 0; i < numOfPoints; i++)
-            {
-                double step = (b - a) / numOfPoints;
-                double x = a + i * step;
-                lagrange[i] = new ObservablePoint(x, Lagrange(x, points));
-            }
-
-            var erf = new ObservablePoint[numOfPoints];
-            for (int i = 0; i < numOfPoints; i++)
-            {
-                double step = (b - a) / numOfPoints;
-                double x = a + i * step;
-                erf[i] = new ObservablePoint(x, Erf(Lagrange(x, points)));
-            }
-
             var error = new ObservablePoint[numOfPoints];
-            double maxError = 0;
+            maxError = 0;
             for (int i = 0; i < numOfPoints; i++)
             {
-                double step = (b - a) / numOfPoints, x = a + i * step;
-                double err = Math.Abs(Taylor(x, eps) - Lagrange(x, points));
-                error[i] = new ObservablePoint(x, err);
+                decimal x = a + i * step;
+
+                newton[i] = new ObservablePoint((double)x, (double)Newton(x, diffs, points));
+                taylor[i] = new ObservablePoint((double)x, (double)Taylor(x));
+                lagrange[i] = new ObservablePoint((double)x, (double)Lagrange(x, points));
+
+                decimal err = Math.Abs(Lagrange(x, points) - Newton(x, diffs, points));
+                error[i] = new ObservablePoint((double)x, (double)err);
                 if (err > maxError) maxError = err;
             }
+
+            label1.Text = numNodes.ToString();
             textBox1.Text = Math.Round(maxError, 9).ToString();
 
             cartesianChart1.ZoomMode = LiveChartsCore.Measure.ZoomAndPanMode.X;
@@ -84,13 +110,13 @@ namespace NumericalMethods2
                     LineSmoothness = 0,
                     GeometrySize = 3
                 },
-                new LineSeries<ObservablePoint>
-                {
-                    Name = "L_n(x)",
-                    Values = lagrange,
-                    LineSmoothness = 0,
-                    GeometrySize = 3
-                }
+                //new LineSeries<ObservablePoint>
+                //{
+                //    Name = "L_n(x)",
+                //    Values = lagrange,
+                //    LineSmoothness = 0,
+                //    GeometrySize = 3
+                //}
                 //new LineSeries<ObservablePoint>
                 //{
                 //    Name = "Error",
@@ -98,59 +124,52 @@ namespace NumericalMethods2
                 //    LineSmoothness = 0,
                 //    GeometrySize = 3
                 //}
-                //new LineSeries<ObservablePoint>
-                //{
-                //    Name = "Newton",
-                //    Values = newton,
-                //    LineSmoothness = 0,
-                //    GeometrySize = 3
-                //}
-                //new LineSeries<ObservablePoint>
-                //{
-                //    Name = "Erf",
-                //    Values = erf,
-                //    LineSmoothness = 0,
-                //    GeometrySize = 3
-                //}
+                new LineSeries<ObservablePoint>
+                {
+                    Name = "Newton",
+                    Values = newton,
+                    LineSmoothness = 0,
+                    GeometrySize = 3
+                }
             };
         }
-        private double Taylor(double x, double accuracy = 0.000001)
+        private decimal Taylor(decimal x, decimal accuracy = 0.000001m)
         {
-            double a = -x * x / 4, sum = a;
-            double n = 1;
+            decimal a = -x * x / 4, sum = a;
+            decimal n = 1;
             while (Math.Abs(a) >= accuracy)
             {
-                a *= -0.5 * n * x * x / (2 * n + 1) / (n + 1) / (n + 1);
+                a *= -0.5m * n * x * x / (2 * n + 1) / (n + 1) / (n + 1);
                 sum += a;
                 n++;
             }
             return sum;
         }
-        private List<List<double>> Tabulate(double a, double b, int numOfPoints, double accuracy = 0.000001)
+        private List<List<decimal>> Tabulate(decimal a, decimal b, int numOfPoints, decimal accuracy = 0.000001m)
         {
             if (numOfPoints < 2) throw new Exception();
-            double step = (b - a) / (numOfPoints - 1);
-            List<List<double>> points = new List<List<double>>();
+            decimal step = (b - a) / (numOfPoints - 1);
+            List<List<decimal>> points = new List<List<decimal>>();
             for (int i = 0; i < numOfPoints; i++)
             {
-                points.Add(new List<double>() { a + i * step, Taylor(a + i * step, accuracy) });
+                points.Add(new List<decimal>() { a + i * step, Taylor(a + i * step, accuracy) });
             }
             return points;
         }
-        private List<List<double>> TabulateChebishev(double a, double b, int numOfPoints, double accuracy = 0.000001)
+        private List<List<decimal>> TabulateChebishev(decimal a, decimal b, int numOfPoints, decimal accuracy = 0.000001m)
         {
             if (numOfPoints < 2) throw new Exception();
-            List<List<double>> points = new List<List<double>>();
+            List<List<decimal>> points = new List<List<decimal>>();
             for (int i = 0; i < numOfPoints; i++)
             {
-                double x = 0.5 * (a + b) + 0.5 * (b - a) * Math.Cos((2 * i + 1) * Math.PI / (2 * numOfPoints));
-                points.Add(new List<double>() { x, Taylor(x, accuracy) });
+                decimal x = (a + b) / 2 + (b - a) / 2 * (decimal)Math.Cos((2 * i + 1) * Math.PI / (2 * numOfPoints));
+                points.Add(new List<decimal>() { x, Taylor(x, accuracy) });
             }
             return points;
         }
-        private double Lagrange(double x, List<List<double>> points)
+        private decimal Lagrange(decimal x, List<List<decimal>> points)
         {
-            double sum = 0, product;
+            decimal sum = 0, product;
             for (int i = 0; i < points.Count; i++)
             {
                 product = points[i][1];
@@ -163,40 +182,32 @@ namespace NumericalMethods2
             }
             return sum;
         }
-        private double Erf(double x)
-        {
-            return 2 / Math.Sqrt(Math.PI) * RectrangleMethod(0, x, Exp);
-        }
-        private double Exp(double x)
-        {
-            return Math.Pow(Math.E, -x * x);
-        }
-        private double RectrangleMethod(double a, double b, Foo f, double eps = 0.0001, double n = 100)
-        {
-            double h = (b - a) / n;
-            double sum = 0;
-            for (int i = 0; i < n; i++)
-            {
-                double x = a + i * h;
-                sum += h * f(x + h / 2);
-            }
+        //private decimal RectrangleMethod(decimal a, decimal b, Foo f, decimal eps = 0.0001, decimal n = 100)
+        //{
+        //    decimal h = (b - a) / n;
+        //    decimal sum = 0;
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        decimal x = a + i * h;
+        //        sum += h * f(x + h / 2);
+        //    }
 
-            h = (b - a) / 2 / n;
-            double dsum = 0;
-            for (int i = 0; i < 2 * n; i++)
-            {
-                double x = a + i * h;
-                dsum += h * f(x + h / 2);
-            }
-            if (Math.Abs(sum - dsum) <= eps) return sum;
-            return RectrangleMethod(a, b, f, eps, 2 * n);
-        }
-        private double DividedDiffs(List<List<double>> points)
+        //    h = (b - a) / 2 / n;
+        //    decimal dsum = 0;
+        //    for (int i = 0; i < 2 * n; i++)
+        //    {
+        //        decimal x = a + i * h;
+        //        dsum += h * f(x + h / 2);
+        //    }
+        //    if (Math.Abs(sum - dsum) <= eps) return sum;
+        //    return RectrangleMethod(a, b, f, eps, 2 * n);
+        //}
+        private decimal DividedDiffs(List<List<decimal>> points)
         {
-            double sum = 0;
+            decimal sum = 0;
             for (int j = 0; j < points.Count; j++)
             {
-                double prod = 1;
+                decimal prod = 1;
                 for (int i = 0; i < points.Count; i++)
                 {
                     if (j == i) continue;
@@ -206,10 +217,10 @@ namespace NumericalMethods2
             }
             return sum;
         }
-        private double Newton(double x, List<double> diffs, List<List<double>> points)
+        private decimal Newton(decimal x, List<decimal> diffs, List<List<decimal>> points)
         {
-            double sum = points[0][1];
-            double prod = 1;
+            decimal sum = points[0][1];
+            decimal prod = 1;
             for (int i = 0; i < points.Count - 1; i++)
             {
                 prod *= x - points[i][0];
